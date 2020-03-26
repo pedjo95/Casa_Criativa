2b6764ffd2f49a4b3e8ffa2b9c59dcbef4cb5ca8
pedjo95 <tec.ped95@gmail.com>
2020-03-25 22:32:55 -0300

    Adicionando a página da lista de ideias

--- scripts.js
+++ scripts.js
@@ -3,5 +3,13 @@ function onOFF() {
       .querySelector("#modal")
       .classList
       .toggle("hide")
+  document
+      .querySelector("body")
+      .classList
+      .toggle("hideScroll")
+  document
+      .querySelector("#modal")
+      .classList
+      .toggle("addScroll")
 }
 
